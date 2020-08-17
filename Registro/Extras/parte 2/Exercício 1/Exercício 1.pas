689c46b903841f098e588e972b28a6ad8eafe38d
evandroase <evandroase@gmail.com>
2020-08-17 07:27:13 -0300

    Exercício 1

---
+++ Registro/Extras/parte 2/Exercício 1/Exercício 1.pas
@@ -0,0 +1,8 @@
+{
+
+Exercício 1: Dados os seguintes campos de um registro:  nome, dia de aniversário e mês de aniversário,
+desenvolver um algoritmo que mostre em cada um dos meses do ano quem são as pessoas que fazem aniversário,
+exibir também o dia. Considere um conjunto de 40 pessoas.
+
+}
+
