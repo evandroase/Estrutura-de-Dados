50a77a61f3da67068f19b4931aae7f9d25197599
evandroase <evandroase@gmail.com>
2020-08-18 09:14:01 -0300

    Create Exercício 3.pas

---
+++ Registro/Extras/parte 2/Exercício 3/Exercício 3.pas
@@ -0,0 +1,13 @@
+{
+
+Exercício 3: Tendo um registro contendo Matrícula do Aluno (MA) do aluno, tipo de participação (A, B, C ou D) e
+Sócio da SBC (S­sim ou N­não), desenvolver um algoritmo para calcular o valor que cada aluno vai pagar para
+participar da semana de informática, sabendo­se que:
+
+Tipo de Participação:  Valor a Pagar:
+A ­ 1 curso              R$  30,00
+B ­ 2 cursos             R$  60,00
+C ­ 3 cursos             R$  90,00
+D ­ outros               R$  100,00
+
+}
