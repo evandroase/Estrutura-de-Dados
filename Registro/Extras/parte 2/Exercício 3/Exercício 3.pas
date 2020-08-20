4fd316a5542db7c2cf57011c46727c6f138e8f30
evandroase <evandroase@gmail.com>
2020-08-19 22:41:59 -0300

    Exercício 3 - Extras - Parte 2

--- Registro/Extras/parte 2/Exercício 3/Exercício 3.pas
+++ Registro/Extras/parte 2/Exercício 3/Exercício 3.pas
@@ -11,3 +11,82 @@ C ­ 3 cursos             R$  90,00
 D ­ outros               R$  100,00
 
 }
+
+Program Cadastro_Aluno;
+uses crt;
+Type
+  cad_aluno = Record
+                matricula: integer;
+                participacao,socio: char;
+              End;
+Var
+  aluno: cad_aluno;
+  funcionamento: boolean;
+  continuar: char;
+  valor: real;
+	
+Begin
+  clrscr;
+  funcionamento := true;
+	
+  while(funcionamento = true)Do
+  Begin
+    valor := 0;
+    writeln('Quanto você vai pagar na Semana de Informática?');
+	  writeln('');
+    writeln('Digite a sua matricula:');
+    readln(aluno.matricula);
+    writeln('');
+    writeln('Socio da SBC?:');
+    readln(aluno.socio);                    
+		while((aluno.socio <> 'S') and (aluno.socio <> 's') and (aluno.socio <> 'N') and (aluno.socio <> 'n'))do
+    Begin
+      writeln('Socio da SBC?');
+      writeln('');
+      writeln('Digite S ou s para socio');
+      writeln('Digite N ou n para não socio.');
+      readln(aluno.socio);  
+    End;
+
+    while(valor = 0)do
+    Begin
+      writeln('');
+      writeln('Digite o tipo de participacao:');
+      writeln('');
+      writeln('A - 1 curso');
+      writeln('B - 2 cursos');
+      writeln('C - 3 cursos');
+      writeln('D - outros cursos');
+      readln(aluno.participacao);
+      case(aluno.participacao)of
+        'A': valor := 30;
+        'B': valor := 60;
+        'C': valor := 90;
+        'D': valor := 100;
+        else
+             writeln('Opcao invalida');
+			End;
+    End;
+		
+    writeln('MA: ', aluno.matricula);
+    writeln('Socio da SBC? ', aluno.socio);
+    writeln('Preco da participacao: R$', valor:0:2);
+    writeln('');
+    writeln('Deseja calcular o preco de um novo participante?');
+    writeln('S - Sim');
+    writeln('N - Nao');
+    readln(continuar);
+		
+  while((continuar <>'S') and (continuar <> 'N'))do
+  Begin
+    writeln('Digite S para sim ou N para nao.');
+    readln(continuar);
+  End;
+	
+  if(continuar = 'S')
+    then funcionamento := true
+    else funcionamento := false;
+  End;
+
+ Repeat Until KeyPressed;
+End.
