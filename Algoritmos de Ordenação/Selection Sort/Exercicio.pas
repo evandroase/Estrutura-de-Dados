0c4b18510b950c798f2a4ba883f28d2fe2d9cf91
evandroase <evandroase@gmail.com>
2020-08-28 15:34:04 -0300

    Update Exercicio.pas

--- Algoritmos de Ordenação/Selection Sort/Exercicio.pas
+++ Algoritmos de Ordenação/Selection Sort/Exercicio.pas
@@ -3,6 +3,35 @@
 Exercício: Escreva um algoritmo que armazene 100 valores num vetor e ordene-os crescentemente implementando o método
 selection sort.
 
+Algoritmo Ordena_Selecionando;
+Var
+  i,j,indice_maior,maior: inteiro;
+  v: vetor[1..100] de inteiro;
+
+Inicio
+  Exiba("Programa que armazena e ordena um vetor de 100 posições.');
+  para i <- 1 ate 100 faca
+    exiba('Digite o ',i,'º numero');
+    leia(v[i]);
+  fimpara;
+  para i <- 1 ate 99 faca
+    maior <- -32768;
+    para j <- 1 ate (101-i) faca
+      se(v{j] > maior)
+        entao Inicio
+                maior <- v[j];
+                indice_maior <- j;
+              Fim;
+    fimpara;
+    v[indice_maior] <- v[101-i];
+    v[101-i] <- maior;
+  fimpara;
+  exiba("O vetor ordenado fica: ");
+  para i <- 1 ate 100 faca
+    exiba(i,"º elemento:",v[i]);
+  fimpara;
+Fim.
+
 }
 
 Program Ordena_Selecionando;
