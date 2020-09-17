95fe0bcee07f73be94e30b6e35cd45ca9e1e6823
evandroase <evandroase@gmail.com>
2020-09-17 14:38:45 -0300

    Cadastri_Lista_Estatica_Sequencial

---
+++ Lista Estática/Sequencial/Cadastro_Lista_Estatica_Seq.pas
@@ -0,0 +1,116 @@
+Program Cadastro_Lista_Estatica_Seq;
+uses crt;
+Const
+  max = 500;
+
+Type
+  cadastro = Record
+               endereco: string;
+               telefone: string;
+               nome: string;
+             End;
+  lista = Record
+            elem: array[1..max] of cadastro;
+            num: Integer;
+          End;
+Var
+  menu: char;
+  L: lista;
+  posicao: Integer;
+
+Procedure menuPrincipal(Var menu:char);                           // 0 - Menu Principal
+Begin
+    writeln('Programa de clientes.');
+    writeln('1 - Criar lista de clientes.');
+    writeln('2 - Conferir se lista está vazia.');
+    writeln('3 - Adicionar um cliente na sequência.');
+    writeln('4 - Adicionar um cliente em uma posição de sua escolha.');
+    writeln('5 - Excluir cliente.');
+    writeln('6 - Verificar quantidade de clientes cadastrados.');
+    writeln('7 - Confere uma posicao.');
+    writeln('8 - Sair.');
+	  readln(menu);
+    ClrScr;
+End;
+
+Procedure criaLista(Var L: lista);                                 // 1 - Criação da lista
+Begin
+  L.num := 0;
+  writeln('Lista criada com sucesso.');
+End;
+
+Function confereLista(L:lista):Boolean;                            // 2 - Confere se lista tem elementos.
+Begin
+  if(L.num = 0)
+    then confereLista := true
+    else confereLista := false;
+End;
+
+Procedure adicionaSeq(Var L: lista);
+Begin                                                              // 3 - Adiciona elemento na sequência.
+  L.num := L.num + 1;
+  writeln('Digite o nome do cliente:');
+  Readln(L.elem[L.num].nome);
+  writeln('Digite o endereco de ',L.elem[L.num].nome,':');
+  Readln(L.elem[L.num].endereco);
+  writeln('Digite o telefone de ',L.elem[L.num].nome,':');
+  Readln(L.elem[L.num].telefone);
+  ClrScr;
+  writeln('Cadastro efetuado com sucesso.');
+End;
+
+Procedure adicionaPosicao(Var L:lista; posicao: Integer);           // 4 - Adiciona elemento em posição indicada pelo usuário.
+Var
+  i: Integer;
+Begin
+  for i:= (L.num) DownTo (posicao + 1) do
+  Begin
+    L.elem[i+1].nome := L.elem[i].nome;
+    L.elem[i+1].endereco := L.elem[i].endereco;
+    L.elem[i+1].telefone := L.elem[i].telefone;
+	End;
+  L.num := L.num + 1;
+  writeln('Digite o nome do cliente:');
+  Readln(L.elem[posicao].nome);
+  writeln('Digite o endereco de ',L.elem[posicao].nome,':');
+  Readln(L.elem[posicao].endereco);
+  writeln('Digite o telefone de ',L.elem[posicao].nome,':');
+  Readln(L.elem[posicao].telefone);
+  ClrScr;
+  writeln('Cadastro efetuado com sucesso.');
+	
+End;
+
+Procedure consultaPosicao(posicao: integer);                         // 7 - Consulta a posição escolhida pelo usuário.
+Begin
+  writeln('Nome: ', L.elem[posicao].nome);
+  writeln('Endereco: ', L.elem[posicao].endereco);
+  writeln('Telefone: ', L.elem[posicao].telefone);
+  writeln('');
+End;
+
+Begin
+  Repeat
+    menuPrincipal(menu);
+    case(menu)of
+      '1': criaLista(L);
+      '2': if(confereLista(L))
+             then writeln('Lista vazia.')
+             else writeln('Lista nao vazia.');
+      '3': adicionaSeq(L);
+      '4': Begin
+             writeln('Digite a posicao que deseja adicionar:');
+             readln(posicao);
+             adicionaPosicao(L,posicao); 
+					 End;
+			'7': Begin
+             writeln('Qual posicao deseja consultar?');
+             readln(posicao); 
+             consultaPosicao(posicao);
+           End;
+	
+    	'8': writeln('Saindo do programa');
+    end;
+	Until (menu = '8');
+  Repeat Until KeyPressed;
+End.
